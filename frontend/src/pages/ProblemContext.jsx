f0322f248d0989115ff57251679b72e260e4868f
hussain-9660 <hussainloha.wala@.fourkitescom>
2024-04-30 20:09:49 +0530

    Added the problem context page

--- frontend/src/pages/ProblemContext.jsx
+++ frontend/src/pages/ProblemContext.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
-import { FaSignInAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 
-function ProblemContext() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+function ProblemContext({ title: title }) {
+  const [codedata, setcodeData] = useState({
+    code: "",
+    langauge: "",
   });
 
-  const { email, password } = formData;
+  const { code, language } = codedata;
+  const solveSuccess = false;
+  const compilerError = false;
+  const failure = false;
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -27,19 +29,10 @@ function ProblemContext() {
   }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
-    setFormData((prevState) => ({
+    setcodeData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const userData = {
-      email,
-      password,
-    };
   };
 
   if (isLoading) {
@@ -49,8 +42,81 @@ function ProblemContext() {
   return (
     <>
       <div id="main">
-        <div id="problem-statement"></div>
-        <div id="editor"></div>
+        <div id="problem">
+          <ul id="problem-heading">
+            <li>{title}</li>
+            <ul>
+              <li>Hard</li>
+              <li>✔Solved</li>
+            </ul>
+          </ul>
+          {solveSuccess ? (
+            <>
+              <div id="success-solve">Success</div>
+              <ul id="navigate-buttons">
+                <li>◀◀ Previous</li>
+                <li>Next ▶▶</li>
+              </ul>
+            </>
+          ) : compilerError ? (
+            <>
+              <div id="compiler-error">Compiler Error</div>
+              <ul id="navigate-buttons">
+                <li>◀◀ Previous</li>
+                <li>Next ▶▶</li>
+              </ul>
+            </>
+          ) : failure ? (
+            <>
+              <div id="failure-solve">Failure</div>
+              <ul id="navigate-buttons">
+                <li>◀◀ Previous</li>
+                <li>Next ▶▶</li>
+              </ul>
+            </>
+          ) : (
+            <>
+              <div id="problem-statement">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad
+                rerum error incidunt. Similique consequuntur consequatur quaerat
+                quasi eius deserunt pariatur ea amet tempora quae minima
+                laudantium, rerum odit animi beatae quos voluptatum unde
+                voluptates nulla cum earum est omnis corrupti voluptatibus.
+                Doloremque a dolorem voluptatem quis ratione modi fuga. Animi.
+              </div>
+              <ul id="problem-specs">
+                <li>Testcase 1</li>
+                <li>Testcase 2</li>
+                <li>Constraints</li>
+              </ul>
+            </>
+          )}
+        </div>
+
+        <div id="editor">
+          <ul id="editor-heading">
+            <li>
+              <select id="langauge-select">
+                <option value="cpp">C++</option>
+                <option value="java">Java</option>
+                <option value="python">Python</option>
+                <option value="javascript">Javascript</option>
+              </select>
+            </li>
+            <ul>
+              <li>Run</li>
+              <li>Submit</li>
+            </ul>
+          </ul>
+
+          <textarea
+            id="codepad"
+            placeholder="Enter your code here"
+            onchange={onChange}
+          ></textarea>
+
+          <div id="code-reset">Reset</div>
+        </div>
       </div>
     </>
   );
