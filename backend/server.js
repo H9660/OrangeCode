4473bc823201dcc4fa308ad6f63da088a6554423
H9660 <hussainloha.wala@.fourkitescom>
2024-05-26 14:47:40 +0530

    Removed more dead code

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,7 @@ app.use("/api/problems", require("./routes/problemRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  // Here we are making sure that the index.html file is passed in the production env 
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
